a1a6934ea03821adb860d9b3a123da197ae2ee01
Sprial404 <1sprial404@gmail.com>
2024-05-19 15:45:10 +0800

    Add copyright notice to project files

--- charlie/bot.py
+++ charlie/bot.py
@@ -2,6 +2,18 @@
 This module contains the code for the Discord bot that will be used to count in the Discord channel.
 """
 
+#  This file is part of charlie.
+#
+#  charlie is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+#  License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+#  later version.
+#
+#  charlie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along with charlie. If not,
+#  see <https://www.gnu.org/licenses/>.
+
 import json
 import os
 import pathlib
